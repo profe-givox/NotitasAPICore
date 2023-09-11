2d11cff23b78cc56e1b55566da8c0ac526e19864
NathGrc <nataliamgrc@gmail.com>
2023-09-11 14:17:01 -0600

    Put Recordatorios

--- webapi/Controllers/RecordatoriosController.cs
+++ webapi/Controllers/RecordatoriosController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Data;
+using System.Runtime.InteropServices;
+using webapi.Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -34,9 +37,35 @@ namespace webapi.Controllers
 
         // PUT api/<RecordatoriosController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Recordatorios actRecordatorios)
         {
+
+            if (actRecordatorios == null)
+            {
+                return BadRequest("No hay datos a actualizar.");
+            }
+
+            var existingRec = new RecordatoriosDAO().GetOneById(id);
+
+            if (existingRec == null)
+            {
+                return NotFound("No se encontro el recordatorio.");
+            }
+
+            existingRec.fecha_recordatorio = actRecordatorios.fecha_recordatorio;
+
+            ulong resultadoEdicion = new RecordatoriosDAO().Editar(existingRec);
+
+            if (resultadoEdicion > 0)
+            {
+                return Ok(existingRec);
+            }
+            else
+            {
+                return StatusCode(500, "Error al actualizar.");
+            }
         }
+
 
         // DELETE api/<RecordatoriosController>/5
         [HttpDelete("{id}")]
--- webapi/Data/RecordatoriosDAO.cs
+++ webapi/Data/RecordatoriosDAO.cs
@@ -1,8 +1,53 @@
-﻿namespace webapi.Data
+﻿using MySqlConnector;
+using webapi.Model;
+using webapi.Util;
+
+namespace webapi.Data
 {
     public class RecordatoriosDAO
     {
         //DESARROLLAR AQUI EL METODO ASIGNADO EN CLASE
+        public ulong Editar(Recordatorios rec)
+        {
+            var ad = new AccesoDatos();
+            using (ad)
+            {
+                ad.parameters.Add(new MySqlParameter("@idRecordatorios", rec.idRecordatorios));
+                ad.parameters.Add(new MySqlParameter("@notitas_id", rec.notitas_id));
+                ad.parameters.Add(new MySqlParameter("@fecha_recordatorio", rec.fecha_recordatorio));
+                ad.sentencia = "UPDATE recordatorios " +
+                                "SET fecha_recordatorio = @fecha_recordatorio" +
+                                "WHERE idRecordatorios = @idRecordatorios";
+                return (ulong)ad.ejecutarSentencia(TIPOEJECUCIONSQL.ESCALAR);
+            }
+        }
 
-    }   
+        public Recordatorios GetOneById(int idRecordatorio)
+        {
+            var ad = new AccesoDatos();
+            Recordatorios recordatorios = null;
+
+            using (ad)
+            {
+                ad.parameters.Add(new MySqlParameter("@idRecordatorios", idRecordatorio));
+                ad.sentencia = "SELECT * FROM Recordatorios WHERE idRecordatorios = @idRecordatorios";
+
+                var reader = (MySqlDataReader)ad.ejecutarSentencia(TIPOEJECUCIONSQL.CONSULTA);
+
+                while (reader.Read())
+                {
+                    recordatorios = new Recordatorios
+                    {
+                        idRecordatorios = reader.GetUInt64("idRecordatorios"),
+                        notitas_id = reader.GetInt32("notitas_id"),
+                        fecha_recordatorio = reader.GetString("fecha_recordatorio"),
+                    };
+                }
+            }
+
+            return recordatorios;
+        }
+
+
+    }
 }
